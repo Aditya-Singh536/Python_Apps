a0a049609b4edc28a79bf448ef43996e1d5d45f9
Aditya-Singh536 <adityasingh536@outlook.com>
2025-07-19 08:45:21 +0530

    Created a todo list

---
+++ Todo List/todo_list.py
@@ -0,0 +1,57 @@
+import fileinput
+import sys
+
+
+def contents(file_name):
+    line_number = 1
+    for line in file_name:
+        print(f"{line_number}: {line.strip()}")
+        line_number += 1
+
+
+def deletion():
+    try:
+        delete_line_number = int(input("\nEnter the line number to delete: "))
+    except ValueError:
+        print("Invalid input. Please enter a number.")
+        sys.exit()
+
+    for line_number, line in enumerate(
+        fileinput.input("items.todo_list.txt", inplace=1), start=1
+    ):
+        if line_number == delete_line_number:
+            continue
+        sys.stdout.write(line)
+    print(f"\nLine {delete_line_number} deleted successfully.")
+
+
+ask = input("Do you wanna add todos? (y/n):").lower()
+
+if ask == "y":
+    with open("items.todo_list.txt", "a") as file_a:
+        ask = input("\nEnter a new task:")
+        file_a.write(f"{ask}\n")
+else:
+    print(f"\nOkay!")
+
+ask = input("\nDo you wanna see your todos? (y/n):").lower()
+
+if ask == "y":
+    with open("items.todo_list.txt", "r") as file_r:
+        print(f"\nThe Todos are:\n")
+        contents(file_r)
+else:
+    print("\nOkay!")
+
+ask = input("\nDo you wanna delete any of the todos? (y/n):")
+print()
+
+if ask == "y":
+    with open("items.todo_list.txt", "r") as file_d:
+        contents(file_d)
+
+    deletion()
+
+    print()
+
+    contents()
