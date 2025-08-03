6fa38c889c3b08ede52f995e1f1c3cbfcf58cf37
Aditya-Singh536 <adityasingh536@outlook.com>
2025-08-03 21:37:30 +0530

    Updated

--- FlappyBird/main.py
+++ FlappyBird/main.py
@@ -8,19 +8,22 @@ fps = 60
 black = (0, 0, 0)
 white = (255, 255, 255)
 gray = (128, 128, 128)
+
 win = pygame.display.set_mode([WIDTH, HEIGHT])
 timer = pygame.time.Clock()
 
 
 # variable library
-player_x = 0
-player_y = 0
+player_x = 225
+player_y = 225
 
 
 def draw_player(x_pos, y_pos):
-    player = pygame.draw.rect(win, white, [x_pos, y_pos, 30, 30])
-
-    return player
+    mouth = pygame.draw.circle(win, gray, [x_pos + 25, y_pos + 15], 12)
+    player = pygame.draw.rect(win, white, [x_pos, y_pos, 30, 30], 0, 12)
+    eye = pygame.draw.circle(win, black, [x_pos + 24, y_pos + 12], 5)
+    jetpack = pygame.draw.rect(win, white, [x_pos - 20, y_pos, 18, 28], 3, 2)
+    return mouth, player, eye, jetpack
 
 
 running = True
@@ -37,4 +40,3 @@ while running:
     pygame.display.flip()
 
 pygame.quit()
-
