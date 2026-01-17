06abc438b7bca423f6406c59ce24c9f3077c0279
Aditya-Singh536 <adityasingh536@outlook.com>
2026-01-17 18:21:47 +0530

    Improvise

--- secretcode.py
+++ secretcode.py
@@ -1,8 +1,15 @@
 def secretcode():
-  actualstring = input('The words to convert to secret code:')
-  print(' ')
-  reversedstring = actualstring[::-1]
-  print(f'Your secret code is: {reversedstring}')
+    actualstring = input("The words to convert to secret code:")
+    print(" ")
+    reversedstring = actualstring[::-1]
+    print(f"Your secret code is: {reversedstring}")
 
-if __name__ == '__main__':
-  secretcode()
+
+secretcode()
+
+ask = input("Do you want to continue? (y/N): ")
+
+if ask == "y":
+    secretcode()
+else:
+    print("Exiting...")
